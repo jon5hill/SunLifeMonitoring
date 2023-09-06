47146f5030e09b0e3da8f1b05e4ad66dc8aed01f
jon5hill <jonathan.hill628@gmail.com>
2023-09-06 00:50:17 -0400

    Changed the endpoint processing to be more general purpose.

--- webServer.js
+++ webServer.js
@@ -9,16 +9,45 @@
  */
 const http = require('http');
 
+/*
+   Function getUserStatusUrl
+
+   This function parses the url sent to the server and returns the path to the directory that will process the
+   request, if it exists.
+
+   @param   userUrl       the URL to parse
+   @return                the path in the URL, excluding the text after the last "/"
+ */
+getUserStatusUrl = function (userUrl) {
+    let ret = './';
+    if (userUrl.indexOf('/') === -1) {
+        // There is no path in the URL
+        return ret;
+    }
+    ret += userUrl.substring(0, userUrl.lastIndexOf('/'));
+    return ret;
+}
+
+
 http.createServer(function (req, res) {
     console.log(req.url);
     try {
-        /* In a valid URL, userStatusUrl will be ./v1/httpGetStatus and userStatusOption will be one of the three
-           endpoints (google-status, amazon-status, or all-status.
+        /* In a valid URL, userStatusUrl will be ./[PATH]/httpGetStatus, where [PATH] is parsed from req.url.
+           userStatusOption will be the endpoint for the path.
+
+           For example: /v1/google-status
+                            userStatusUrl = './v1/httpGetStatus'
+                            userStatusOption = 'google-status'
+                        /v2/americas/business-unit-5/verizon-status
+                            userStatusUrl = './v2/americas/business-unit-5/httpGetStatus'
+                            userStatusOption = 'verizon-status'
+
            If the URL is not valid, it will throw an exception which will return an error status, or pass an undefined
            endpoint to httpGetStatus() which will return an error status.
          */
-        let userStatusUrl = './' + req.url.split('/')[1] + '/httpGetStatus';
-        let userStatusOption = req.url.split('/v1/')[1];
+        let userStatusUrl = getUserStatusUrl(req.url) + '/httpGetStatus';
+        let splitUrl = req.url.split('/');
+        let userStatusOption = splitUrl[splitUrl.length - 1];
         const userStatusRequest = require(userStatusUrl);
         userStatusRequest.httpGetStatus(userStatusOption, res);
     }
